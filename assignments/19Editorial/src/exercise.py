31ccadce2fa795543df09015d3411057750cd39b
bryanmojicaa <89592248+bryanmojicaa@users.noreply.github.com>
2021-08-30 14:46:48 -0500

    tarea

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,13 @@
+import math
 def main():
     # escribe tu código abajo de esta línea
-    pass
 
+
+    Número_de_palabras= int(input("Dame el número de palabras: "))  
+
+    calculo= math.ceil((Número_de_palabras/475)*(60)*(0.90))
+
+    print("El costo de la publicación es:",calculo)
 
 if __name__ == '__main__':
     main()
