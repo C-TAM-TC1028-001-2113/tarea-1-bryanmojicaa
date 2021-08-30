dde535f98c756b7cde3322f28210921ec0006905
bryanmojicaa <89592248+bryanmojicaa@users.noreply.github.com>
2021-08-30 10:24:04 -0500

    tarea

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -3,6 +3,7 @@ def main():
     Gramos_de_harina= float(input("Dame la harina en gramos: "))
 
     calculo= (Gramos_de_harina)*(0.050)
+    calculo= round(calculo,1)
 
     print("Necesitas estos gramos de levadura:",calculo)
     
